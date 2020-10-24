2d654a238c9d30664d5c3385436e2dda5b1d4939
Manikandan Uthaman <manikandan.mit@outlook.com>
2020-10-24 21:11:39 +0530

    Implemented home page

--- readme-gui/src/app/app.module.ts
+++ readme-gui/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
